fa4e27fcea3b8e39e37c81316f0f2a3127687410
Lenny <lenny.brwn@mail.ru>
2024-03-31 17:15:45 +0500

    hey

---
+++ src/store/UserStore.js
@@ -0,0 +1,36 @@
+import { makeAutoObservable } from "mobx";
+//import { makeAutoObservable } from "mobx";
+class UserStore {
+  users = [];
+  mainUser = {
+    id: null,
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  };
+  isLoading = false;
+
+  constructor() {
+    makeAutoObservable(this);
+    this.isLoading = false;
+  }
+  getUser(id) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  //   async fetchUsers() {
+  //     try {
+  //       this.isLoading = true;
+  //       const res = await getUsers();
+  //       console.log("полученные пользователи", res.data); //?
+  //       this.users = res.data;
+  //     } catch (error) {
+  //       console.error("Error fetching users:", error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   }
+}
+
+export default UserStore;
